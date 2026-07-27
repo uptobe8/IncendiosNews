554c985c7de422b600a2fe2db3e8af49bea98b40
uptobe8 <ia.uptobe@gmail.com>
2026-07-27 17:17:06 +0200

    uu

--- start.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-py -m pip install -q -r requirements.txt 2>nul || python -m pip install -q -r requirements.txt
-start "" http://127.0.0.1:8000
-py server.py 2>nul || python server.py
-pause
